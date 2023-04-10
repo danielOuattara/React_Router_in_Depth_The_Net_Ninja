94acd4f3b12655bf7a34e81ca3d3b9a4a1509abc
danielOuattara <daniel.ouattara@gmx.com>
2023-04-10 22:27:52 +0200

    07 routes parameters

---
+++ 07_route_parameters/src/App.jsx
@@ -0,0 +1,52 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import {
+  Home,
+  About,
+  Faq,
+  Contact,
+  NotFound,
+  Careers,
+  careersLoader,
+  CareerDetails,
+  singleCareerLoader,
+} from "./pages";
+import { RootLayout, HelpLayout, CareersLayout } from "./layout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+
+      <Route path="about" element={<About />} />
+
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+
+      <Route path="careers" element={<CareersLayout />}>
+        <Route index element={<Careers />} loader={careersLoader} />
+        <Route
+          path=":careerId"
+          element={<CareerDetails />}
+          loader={singleCareerLoader}
+        />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Route>,
+  ),
+);
+
+export default function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
