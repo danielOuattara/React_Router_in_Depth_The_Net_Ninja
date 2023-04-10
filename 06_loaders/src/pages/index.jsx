ea7f8b0fb2f1cd066eddedf339e67324970201b6
danielOuattara <daniel.ouattara@gmx.com>
2023-04-10 22:27:40 +0200

    06 react router dom loader functions

---
+++ 06_loaders/src/pages/index.jsx
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import About from "./About";
+import Faq from "./help/FAQ";
+import Contact from "./help/Contact";
+import NotFound from "./NotFound";
+import Careers from "./careers/Careers";
+import { careersLoader } from "./careers/Careers";
+
+export { Home, About, Faq, Contact, NotFound, Careers, careersLoader };
