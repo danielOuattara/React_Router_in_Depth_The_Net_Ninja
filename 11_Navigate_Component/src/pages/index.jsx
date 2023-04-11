e9f409bb1d7404b4feadfdd162eb26d0df447b9e
danielOuattara <daniel.ouattara@gmx.com>
2023-04-11 21:11:44 +0200

    11 navigate component

---
+++ 11_Navigate_Component/src/pages/index.jsx
@@ -0,0 +1,25 @@
+import Home from "./Home";
+import About from "./About";
+import Faq from "./help/Faq";
+import Contact from "./help/Contact";
+import NotFound from "./NotFound";
+import Careers from "./careers/Careers";
+import { careersLoader } from "./careers/Careers";
+import CareerDetails from "./careers/CareerDetails";
+import { singleCareerLoader } from "./careers/CareerDetails";
+import CareerError from "./careers/CareerError";
+import { contactAction } from "./help/Contact";
+
+export {
+  Home,
+  About,
+  Faq,
+  Contact,
+  NotFound,
+  Careers,
+  careersLoader,
+  CareerDetails,
+  singleCareerLoader,
+  CareerError,
+  contactAction,
+};
